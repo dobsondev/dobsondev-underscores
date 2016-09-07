0256d504efb39150344848dcb1536a62f720a71b
Alex Dobson <alex.dobson@live.com>
2016-09-07 15:13:00 -0600

    Modified the id and classes in the header and made corresponding changes in style.css

--- _s-original/header.php
+++ _s-original/header.php
@@ -21,17 +21,17 @@
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_sSs' ); ?></a>
 
-	<div id="masthead-wrapper">
-		<header id="masthead" class="site-header row" role="banner">
-			<div class="site-branding small-12 large-6 columns">
+	<header id="top-header" class="site-header" role="banner">
+		<div class="row">
+			<div id="top-header-branding" class="site-branding small-12 large-6 columns">
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			</div><!-- .site-branding -->
 
-			<nav id="site-navigation" class="main-navigation small-12 large-6 columns" role="navigation">
+			<nav id="top-header-nav" class="site-navigation small-12 large-6 columns" role="navigation">
 				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_sSs' ); ?></button>
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 			</nav><!-- #site-navigation -->
-		</header><!-- #masthead -->
-	</div><!-- #masthead-wrapper -->
+		</div><!-- .row -->
+	</header><!-- #top-header -->
 
 	<div id="content">
