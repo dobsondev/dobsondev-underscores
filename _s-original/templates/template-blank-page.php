6214c9ba536f33092ace04c49fb33862de0665f2
Alex Dobson <alex.dobson@live.com>
2017-05-30 12:50:37 -0600

    Added blank page template.

---
+++ _s-original/templates/template-blank-page.php
@@ -0,0 +1,36 @@
+<?php
+/*
+  Theme Name: _SsS
+  Template Name: Blank Page
+*/
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+  <body id="blank-page">
+
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+          <header class="entry-header">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+          </header><!-- .entry-header -->
+
+          <div class="entry-content">
+            <?php the_content(); ?>
+          </div><!-- .entry-content -->
+
+        </article><!-- #post-## -->
+
+      <?php endwhile; // end of the loop. ?>
+
+    <?php wp_footer(); ?>
+  </body>
+</html>
