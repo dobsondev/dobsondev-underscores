f3f5de1352dbf5646a06baa774ba7457d30e6fb9
Alex Dobson <alex.dobson@live.com>
2017-05-30 11:11:27 -0600

    Added Full Width Page template.

---
+++ _s-original/templates/template-full-width-page.php
@@ -0,0 +1,29 @@
+<?php
+/*
+  Theme Name: _SsS
+  Template Name: Full Width Page
+*/
+get_header(); ?>
+
+  <div id="page-primary" class="site-primary">
+    <div class="row">
+      <main id="page-main" class="site-main small-12 columns" role="main">
+
+        <?php while ( have_posts() ) : the_post(); ?>
+
+          <?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+          <?php
+            // If comments are open or we have at least one comment, load up the comment template
+            if ( comments_open() || get_comments_number() ) :
+              comments_template();
+            endif;
+          ?>
+
+        <?php endwhile; // end of the loop. ?>
+
+      </main><!-- #page-main -->
+    </div><!-- .row -->
+  </div><!-- #page-primary -->
+
+<?php get_footer(); ?>
