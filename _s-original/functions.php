38af640d4e9a391af0148ff2b31a8d29223c2062
Alex Dobson <alex.dobson@live.com>
2020-08-17 12:18:16 -0600

    Added Foundation grid shortcodes for use in the editor allowing you to use the grid within the WordPress editor.

--- _s-original/functions.php
+++ _s-original/functions.php
@@ -171,6 +171,42 @@ function _sSs_admin_scripts() {
 add_action( 'admin_enqueue_scripts', '_sSs_admin_scripts' );
 
 /**
+ * Shortcode for foundation grid-container div
+ * [grid-container] and [/grid-container]
+ *
+ * https://codex.wordpress.org/Shortcode_API
+ */
+function foundation_grid_container_shortcode( $atts, $content = NULL ) {
+	return '<div class="grid-container">' . do_shortcode( $content ) . '</div>';
+}
+add_shortcode( 'grid-container', 'foundation_grid_container_shortcode' );
+
+/**
+ * Shortcode for foundation grid-x div
+ * [grid-x] and [/grid-x]
+ *
+ * https://codex.wordpress.org/Shortcode_API
+ */
+function foundation_grid_x_shortcode( $atts, $content = NULL ) {
+	return '<div class="grid-x">' . do_shortcode( $content ) . '</div>';
+}
+add_shortcode( 'grid-x', 'foundation_grid_x_shortcode' );
+
+/**
+ * Shortcode for foundation cell div
+ * [cell classes="small-12 medium-6 large-4"] and [/cell]
+ *
+ * https://codex.wordpress.org/Shortcode_API
+ */
+function foundation_cell_shortcode( $atts, $content = NULL ) {
+	$attributes = shortcode_atts( array(
+		'classes' => 'small-12',
+	), $atts );
+	return '<div class="' . $attributes['classes'] .' cell">' . do_shortcode( $content ) . '</div>';
+}
+add_shortcode( 'cell', 'foundation_cell_shortcode' );
+
+/**
  * Implement the Custom Header feature.
  */
 //require get_template_directory() . '/inc/custom-header.php';
